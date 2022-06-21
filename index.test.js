6209384a12104472d334c33496e665eb0a5a4b78
Simon Lercher <if20b117@technikum-wien.at>
2022-06-21 22:48:39 +0200

    test

--- index.test.js
+++ index.test.js
@@ -1,2 +1,9 @@
-const sum = require('./index');
+const get = require ('./index');
+const express = require('express'); //Import the express dependency
+const app = express();              //Instantiate an express app, the main work horse of this server
+const port = 5000;      
 
+test('checks if server responds', () => {
+    const app = express();
+    expect(app.get('./')).not.toBeNull();
+})
