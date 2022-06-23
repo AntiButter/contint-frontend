386465dda6ed6fb499503a1b218454e41bd65043
Simon Lercher <if20b117@technikum-wien.at>
2022-06-23 17:10:26 +0200

    fail lint

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-//const get = require ('./index');
+const get = require ('./index');
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server     
 test('checks if server responds', () => {
